ee4c12d739a8f61bd29cdd77937e377e2f9eb8b7
santiagocortes <cortescabrerasantiago@gmail.com>
2022-11-02 11:00:04 -0500

    Creation of AbstractModel

---
+++ Modelos/AbstractModelo.py
@@ -0,0 +1,10 @@
+from abc import ABCMeta
+
+#Manipular objetos de manera independiente para nuestro modelo
+class AbstractModelo(metaclass=ABCMeta):
+    #data hace referencia a un diccionario, self es una palabra reservada para acceder a atributos y metodos de una clase
+    def __init__(self, data):
+        #lee items del elemento data (llave y valor)
+        for llave, valor in data.items():
+            #Establece el valor a un atributo como una cadena
+            setattr(self, llave, valor)
