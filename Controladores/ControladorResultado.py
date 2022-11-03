a2943ce321f2da2cc1c68b52a4ec376e847f5353
“valegdlb4” <valegdlb4@gmail.com>
2022-11-02 19:39:48 -0500

    valen side

---
+++ Controladores/ControladorResultado.py
@@ -0,0 +1,43 @@
+from Modelos.Resultado import Resultado
+from Modelos.Mesa import Mesa
+from Modelos.Candidato import Candidato
+from Modelos.Partido import Partido
+from Repositorios.RepositorioResultado import RepositorioResultado
+from Repositorios.RepositorioMesa import RepositorioMesa
+from Repositorios.RepositorioPartido import RepositorioPartido
+from Repositorios.RepositorioCandidato import RepositorioCandidato
+
+class ControladorResultado():
+
+    def __init__(self):
+        self.repositorioResultado = RepositorioResultado()
+        self.repositorioMesa = RepositorioMesa()
+        self.repositorioPartido = RepositorioPartido()
+
+    def index(self):
+        return self.repositorioResultado.findAll()
+
+    def create(self, infoResultados, id_mesa, id_candidato):
+        nuevoResultado = Resultado(infoResultados)
+        laMesa = Mesa(self.repositorioMesa.findById(id_mesa))
+        elCandidato = Candidato(self.repositorioCandidato.findById(id_candidato))
+        nuevoResultado.mesa = laMesa
+        nuevoResultado.candidato = elCandidato
+        return self.repositorioResultado.save()
+
+    def show(self, id):
+        elResultado = Resultado(self.repositorioResultado.findById(id))
+        return elResultado.__dict__
+
+    def update(self, id, infoResultados, id_mesa, id_candidato):
+        elResultado = Resultado(self.repositorioResultado.findById(id))
+        elResultado.Numero_mesa = infoResultados["Numero_mesa"]
+        elResultado.id_partido = infoResultados["id_partido"]
+        laMesa = Mesa(self.repositorioMesa.findById(id_mesa))
+        elCandidato = Candidato(self.repositorioCandidato.findById(id_candidato))
+        elResultado.mesa = laMesa
+        elResultado.candidato = elCandidato
+        return self.repositorioResultado.save(elResultado)
+
+    def delete(self, id):
+        return self.repositorioResultado.delete(id)
--- main.py
+++ main.py
@@ -7,6 +7,12 @@ import json
 
 app = Flask(__name__)
 cors = CORS(app)
+from Controladores.ControladorPartido import ControladorPartido
+from Controladores.ControladorResultado import ControladorResultado
+
+miControladorPartido = ControladorPartido()
+miControladorResultado = ControladorResultado()
+
 
 @app.route("/",methods=['GET'])
 def test():
@@ -20,6 +26,61 @@ def loadFileConfig():
         data = json.load(f)
     return data
 
+#------------------------------Edit partidos -----------------------------------------------------------------
+
+
+@app.route("/partidos",methods=['GET'])
+def getPartidos():
+    json = miControladorPartido.index()
+    return jsonify(json)
+
+@app.route("/partidos",methods=['POST'])
+def crearPartido():
+    data = request.get_json()
+    json = miControladorPartido.create(data)
+    return jsonify(json)
+
+@app.route("/partidos/<string:id>",methods=['GET'])
+def getPartido(id):
+    json=miControladorPartido.show(id)
+    return jsonify(json)
+
+@app.route("/partidos/<string:id>",methods=['PUT'])
+def modificarPartido(id):
+    data = request.get_json()
+    json=miControladorPartido.update(id,data)
+    return jsonify(json)
+
+@app.route("/estudiantes/<string:id>",methods=['DELETE'])
+def eliminarPartido(id):
+    json=miControladorPartido.delete(id)
+    return jsonify(json)
+
+
+#------------------------------Edit Resultados -----------------------------------------------------------------
+
+@app.route("/Resultados",methods=['GET'])
+def getResultados():
+    json=miControladorResultado.index()
+    return jsonify(json)
+@app.route("/Resultados/<string:id>",methods=['GET'])
+def getResultado(id):
+    json=miControladorResultado.show(id)
+    return jsonify(json)
+@app.route("/Resultados/mesa/<string:id_mesa>/candidato/<string:id_candidato>",methods=['POST'])
+def crearResultado(id_estudiante,id_materia):
+    data = request.get_json()
+    json=miControladorResultado.create(data,id_estudiante,id_materia)
+    return jsonify(json)
+@app.route("/Resultados/<string:id_resultado>/mesa/<string:id_mesa>/candidato/<string:id_candidato>",methods=['PUT'])
+def modificarResultado(id_resultado,id_mesa,id_candidato):
+    data = request.get_json()
+    json=miControladorResultado.update(id_resultado,data,id_mesa,id_candidato)
+    return jsonify(json)
+@app.route("/Resultados/<string:id_resultado>",methods=['DELETE'])
+def eliminarResultado(id_resultado):
+    json=miControladorResultado.delete(id_resultado)
+    return jsonify(json)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     dataConfig = loadFileConfig()
