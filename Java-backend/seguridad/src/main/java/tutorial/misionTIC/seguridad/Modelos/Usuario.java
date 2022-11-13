6414b764ceac5b9551679f2dd54f107af7e0d046
santiagocortes <cortescabrerasantiago@gmail.com>
2022-11-12 22:28:04 -0500

    First Commit

--- Java-backend/seguridad/src/main/java/tutorial/misionTIC/seguridad/Modelos/Usuario.java
+++ Java-backend/seguridad/src/main/java/tutorial/misionTIC/seguridad/Modelos/Usuario.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+//Changes
 @Data
 @Document()
 public class Usuario {
