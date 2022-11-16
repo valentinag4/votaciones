45120870e75ba9b5f2a14e6e2977af81fd55fb29
santiagocortes <cortescabrerasantiago@gmail.com>
2022-11-15 20:37:14 -0500

    Rol to user

--- Java-backend/seguridad/src/main/java/tutorial/misionTIC/seguridad/Modelos/Usuario.java
+++ Java-backend/seguridad/src/main/java/tutorial/misionTIC/seguridad/Modelos/Usuario.java
@@ -2,6 +2,7 @@ package tutorial.misionTIC.seguridad.Modelos;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 //Create Desarrollo's branch
@@ -13,6 +14,9 @@ public class Usuario {
     private String seudonimo;
     private String correo;
     private String contrasena;
+
+    @DBRef
+    private Rol rol;
 
     public Usuario(String seudonimo, String correo, String contrasena) {
         this.seudonimo = seudonimo;
