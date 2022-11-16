8ab8d52a41109d6adfe3612b71a66c5e7e2f4da9
santiagocortes <cortescabrerasantiago@gmail.com>
2022-11-15 21:16:52 -0500

    Permiso's controller and repository creation

---
+++ Java-backend/seguridad/src/main/java/tutorial/misionTIC/seguridad/Controladores/ControladorPermiso.java
@@ -0,0 +1,4 @@
+package tutorial.misionTIC.seguridad.Controladores;
+
+public class ControladorPermiso {
+}
---
+++ Java-backend/seguridad/src/main/java/tutorial/misionTIC/seguridad/Repositorios/RepositorioPermiso.java
@@ -0,0 +1,4 @@
+package tutorial.misionTIC.seguridad.Repositorios;
+
+public interface RepositorioPermiso {
+}
